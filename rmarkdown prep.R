fe454201dfe2d0856b775280d56b8d066791346f
Ben Harley <harleyb101010@gmail.com>
2024-10-19 12:26:01 +0100

    flextable stuff

---
+++ rmarkdown prep.R
@@ -0,0 +1,19 @@
+library("flextable")
+
+# Set up some formatting for flextable to be applied to most tables
+flextable_format <- function(data) {
+  data %>%
+    flextable() |>
+    bold(part = "header") %>%
+    bg(bg = "#00008B", part = "header") %>%
+    color(color = "white", part = "header") %>%
+    align(align = "left", part = "header") %>%
+    valign(valign = "center", part = "header") %>%
+    valign(valign = "top", part = "body") %>%
+    colformat_num(big.mark = ",") %>%
+    fontsize(size = 12, part = "all") %>%
+    font(fontname = "Arial", part = "all") %>%
+    border(border = fp_border_default(color = "#000000", width = 0.5), part = "all") |>
+    autofit()
+}
+
