70a7758eedeba273bcc502786aea5f49dbb75ed7
Ben Harley <harleyb101010@gmail.com>
2024-10-19 14:02:07 +0100

    bar_graph_tier0 added

--- rmarkdown prep.R
+++ rmarkdown prep.R
@@ -1,5 +1,5 @@
 library("flextable")
-
+library("ggplot2")
 # Set up some formatting for flextable to be applied to most tables
 flextable_format <- function(data) {
   data %>%
@@ -25,6 +25,15 @@ Faction_Roster_List_Tier0_Table <- Faction_Roster_List_Tier0 %>%
 
 Faction_Roster_List_Tier1_Table <- Faction_Roster_List_Tier1 %>% 
   flextable_format()
+
+bar_graph_tier0_activity <- ggplot(data =  Faction_Roster_Activity_Tier_0_Count,aes( x = Activity_Type, y = count ))+
+  geom_bar(stat="identity")
+
+ggsave("C:/Users/harle/OneDrive/Desktop/SAPD Files 2024/tier0.png",
+       plot = bar_graph_tier0_activity,
+       height = 10,
+       width = 14,
+       dpi = 300)
 
 rmarkdown::render(
   "activity report.Rmd")
